804ca1c7bcb63c1adff53b69ece86ad305b99c55
ぱきです <89261377+pakisophia@users.noreply.github.com>
2022-09-05 16:33:38 +0900

    Add files via upload

---
+++ deque.py
@@ -0,0 +1,14 @@
+from collections import deque
+
+d=deque() #dequeオブジェクトを生成
+
+###append###
+d.appendleft() #左端に要素をappend
+d.append() #通常通り右端に要素をappend
+
+###pop###
+d.popleft() #左端の要素をpop
+d.pop() #通常通り右端の要素をpop
+
+
+
