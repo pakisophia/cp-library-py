4f54cfd33e94397726498ce6491c50512801c68f
ぱきです <89261377+pakisophia@users.noreply.github.com>
2022-11-07 11:01:46 +0900

    Add files via upload

---
+++ soinsubunkai.py
@@ -0,0 +1,18 @@
+#https://note.nkmk.me/python-prime-factorization/
+#あるnを入力した時、nの素因数のlistを返す
+
+def prime_factorize(n):
+    a = []
+    while n % 2 == 0:
+        a.append(2)
+        n //= 2
+    f = 3
+    while f * f <= n:
+        if n % f == 0:
+            a.append(f)
+            n //= f
+        else:
+            f += 2
+    if n != 1:
+        a.append(n)
+    return a
