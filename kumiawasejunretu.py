b09aada2993f63018ea58cd7d9827171243f2672
ぱきです <89261377+pakisophia@users.noreply.github.com>
2022-12-07 01:09:54 +0900

    Update kumiawasejunretu.py

--- kumiawasejunretu.py
+++ kumiawasejunretu.py
@@ -13,11 +13,11 @@ def comb(a,b):
     #aCbの値を返す
 
 #順列列挙
-c=itertools.permutations(l,2)
+c=list(itertools.permutations(l,2))
 #この場合4P2(=12個)の順列がlist(c)に格納される
 
 #組み合わせ集合を列挙
-cc=itertools.combinations(l,2)
+cc=list(itertools.combinations(l,2))
 #この場合4C2(=6個)の組み合わせがlist(cc)に格納される
 
 
