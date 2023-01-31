a936dff0406b20e86381dbef49db24541d936355
ぱきです <89261377+pakisophia@users.noreply.github.com>
2023-01-31 22:32:11 +0900

    Update extgcd.py

--- extgcd.py
+++ extgcd.py
@@ -1,7 +1,8 @@
 #https://tjkendev.github.io/procon-library/python/math/gcd.html
 
 #拡張ユークリッドの互除法
-#ax+by=gcd(a,b) なる(x,y)。又gcd(a,b)を返す。
+#ax+by=gcd(a,b)なる(x,y)を求める。
+#第一返り値としてgcd(a,b)。第二、三返り値として(x,y)を返す。
 
 def extgcd(a, b):
     if b:
